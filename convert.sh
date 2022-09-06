0810b92d8707af81832e1d0edc693a676b2b6f95
root <root@localhost.localdomain>
2022-09-06 04:54:05 +0000

    Debuging is done. Program works

--- convert.sh
+++ convert.sh
@@ -17,7 +17,7 @@ prefix_size=${#prefix}
 postfix=".git"
 postfix_index=-1
 postfix_size=${#postfix}
-
+has_postfix=0
 
 
 
@@ -28,7 +28,7 @@ ssh="git@github.com:"
 #	echo "There is no kind of github repository link. Program terminates."
 #	exit 0
 #fi
-
+#(( 1 > 0 )) && { echo "Only 1" ; } || { echo "Only 0" ; }
 [ -z $https_adr ] && { echo "There is no kind of github repository link. Program terminates." ; exit 0 ; }
 
 #if [ $prefix == ${https_adr:0:19} ]; then
@@ -49,10 +49,10 @@ temp=${end#*.}
 #fi 
 [ $prefix_index -eq "-1" ] && { echo "The inputted line is not github repository link. Program terminates." ; exit 2 ; }
 
-[ $end == $temp ] && { echo "There is no .git addition. Adding..." ; }
-[ $end != $temp ] && {  [ $postfix != ".$temp" ] && { echo "This link is not github repository. Program terminates." ; exit 1 ; } ; }
+[ $end == $temp ] && { echo "There is no .git addition. Adding..." ; has_postfix=1 ; }
+[ $end != $temp ] && { [ $postfix != ".$temp" ] && { echo "This link is not github repository. Program terminates." ; exit 1 ; } || { echo "There are .git addition"; postfix_index=$(($https_adr_size-$postfix_size-1)) ; } ; }
 
-[ $postfix_index -eq "-1" ] && { postfix_index=$post_index_end ; }
+[ $postfix_index -eq "-1" ] && { postfix_index=$(($https_adr_size-1)) ; }
 
 #if [ $postfix != ".$temp" ] && [$end != $temp]; then 
 #	echo "This link is not github repository. Program terminates."
@@ -82,14 +82,19 @@ temp=${end#*.}
 #fi
 
 #temp=${https_tes%%$postfix*} && indexOf=$(echo "https_test%%$postfix*" | cat ${#temp})
-
+[ $has_postfix == "1" ] && { repo=${https_adr:$prefix_size} ; } || { 
+repo=${https_adr:$prefix_size:(($https_adr_size-$prefix_size-$postfix_size))} ; }
+#repo=${https_adr:$prefix_size:15}
+echo $ssh$repo$postfix
 echo -e "Original link:\n"$https_adr
-echo ${#https_adr}
+echo -e "Original link size:\n"${#https_adr}
 echo $prefix
 echo $prefix_index
 echo $prefix_size
+echo -e "Link has postfix:\n"$add_postfix
 echo $postfix
 echo $postfix_index
 echo $postfix_size
 echo $ssh
 echo $((${#https_adr}-${#postfix}))
+echo $repo
